5658091c70228d2f50bce410d639981d1a1d4cbd
williams <williams.lima989@gmail.com>
2023-07-19 15:54:27 -0300

    Inclusão da validação de formulario.js e area de experiencias

---
+++ validate.js
@@ -0,0 +1,19 @@
+function validaformulario(evento){
+    var nome = document.getElementById("nome").Value;
+    var email = document.getElementById("email").value;
+    var assunto = document.getElementById("assunto").value;
+    var mensagem = document.getElementById("mensagem").value;
+
+    if (nome == "" || email == "" || assunto == "" || mensagem == ""){
+        evento.preventDefault()
+        alert("Por favor, preencha todos os campos.");
+
+    } else {
+        alert('Formulario encaminhado!')
+    }
+
+}
+
+const botao = document.getElementById('btn_enviar');
+
+botao.addEventListener('click', validaformulario);
