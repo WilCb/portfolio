07a6f8a22429006e85bda04943e6446ff16bc102
williams <williams.lima989@gmail.com>
2023-07-26 00:06:44 -0300

    correção do scritp.js

--- validate.js
+++ validate.js
@@ -1,18 +1,10 @@
-//função para validação do formulario
-
 function validaformulario(evento){
-
-    // pega os campos de input do html e valor inserido nele
     var nome = document.getElementById("nome").Value;
     var email = document.getElementById("email").value;
     var assunto = document.getElementById("assunto").value;
     var mensagem = document.getElementById("mensagem").value;
 
-    //junta todos em uma única variavel
-    var inputs = [nome, email, assunto, mensagem]
-
-    //se algum campo estiver vazio, pede para preencher
-    if (inputs.length == ""){
+    if (nome == "" || email == "" || assunto == "" || mensagem == ""){
         evento.preventDefault()
         alert("Por favor, preencha todos os campos.");
 
@@ -21,9 +13,7 @@ function validaformulario(evento){
     }
 
 }
-//busca o botão no enviar
+
 const botao = document.getElementById('btn_enviar');
 
-
-//ao evento de clicl no botão envia o formulario
 botao.addEventListener('click', validaformulario);
