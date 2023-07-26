ddc4252eb64b3eb6a92ab7ea72815dfb7479d8d2
williams <williams.lima989@gmail.com>
2023-07-25 23:22:35 -0300

    melhorando legibilidade do codigo JS

--- validate.js
+++ validate.js
@@ -1,9 +1,17 @@
+//função para validação do formulario
+
 function validaformulario(evento){
+
+    // pega os campos de input do html e valor inserido nele
     var nome = document.getElementById("nome").Value;
     var email = document.getElementById("email").value;
     var assunto = document.getElementById("assunto").value;
     var mensagem = document.getElementById("mensagem").value;
+
+    //junta todos em uma única variavel
     var inputs = [nome, email, assunto, mensagem]
+
+    //se algum campo estiver vazio, pede para preencher
     if (inputs.length == ""){
         evento.preventDefault()
         alert("Por favor, preencha todos os campos.");
@@ -13,7 +21,9 @@ function validaformulario(evento){
     }
 
 }
-
+//busca o botão no enviar
 const botao = document.getElementById('btn_enviar');
 
+
+//ao evento de clicl no botão envia o formulario
 botao.addEventListener('click', validaformulario);
