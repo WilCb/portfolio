54188c17f5d539812b03c4c7a13ff45fdd550716
Williams <williams.lima989@gmail.com>
2025-03-20 14:27:45 -0300

    enviar mensgem para netlify

---
+++ assets/js/enviarMensagem.js
@@ -0,0 +1,17 @@
+const handleSubmit = event => {
+    event.preventDefault();
+  
+    const myForm = event.target;
+    const formData = new FormData(myForm);
+  
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString()
+    })
+      .then(() => console.log("Form successfully submitted"))
+      .catch(error => alert(error));
+  };
+  
+  document.querySelector("form").addEventListener("submit", handleSubmit);
+  
